038b5b116b77a0c1b8ae2d9b63702925f4fc7aa6
nurulamin092 <mnurulamin092@gmial.com>
2022-06-27 09:55:33 +0600

    dropdwon added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = ({ children }) => {
     return (
@@ -7,7 +7,7 @@ const Navbar = ({ children }) => {
             <div className="drawer drawer-end">
                 <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col">
-                    <div className="w-full navbar bg-base-300">
+                    <div className="w-full navbar bg-base-100 px-24">
                         <div className="flex-1 px-2 mx-2 text-2xl">Clean Co.</div>
                         <div className="flex-none lg:hidden">
                             <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
@@ -15,12 +15,22 @@ const Navbar = ({ children }) => {
                             </label>
                         </div>
                         <div className="flex-none hidden lg:block">
-                            <ul className="menu menu-horizontal">
-                                <li><Link to="/">Home</Link></li>
-                                <li><Link to="about">About</Link></li>
-                                <li><Link to="contract">Contract</Link></li>
-                                <li><Link to="service">Service</Link></li>
-                                <li><Link to="login">Login</Link></li>
+                            <ul className="menu menu-horizontal gap-x-3">
+                                <li><NavLink to="/" className="rounded-lg">Home</NavLink></li>
+                                <li><NavLink to="about" className="rounded-lg">About</NavLink></li>
+                                <li><NavLink to="contract" className="rounded-lg">Contract</NavLink></li>
+                                <li><NavLink to="service" className="rounded-lg">Service</NavLink></li>
+                                <li><NavLink to="login" className="rounded-lg">Login</NavLink></li>
+                                <li className='dropdown dropdown-hover dropdown-end'>
+                                    <label tabindex="0" class="btn btn-primary btn-outline rounded-lg m-1">
+                                        Book Now
+                                    </label>
+                                    <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                                        <li><a>Item 1</a></li>
+                                        <li><a>Item 2</a></li>
+                                    </ul>
+
+                                </li>
 
                             </ul>
                         </div>
@@ -30,8 +40,8 @@ const Navbar = ({ children }) => {
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
                     <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="about">About</Link></li>
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="about">About</NavLink></li>
 
                     </ul>
 
