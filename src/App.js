9eab36a98efa566081aac934a2609bda737fd1b3
nurulamin092 <mnurulamin092@gmial.com>
2022-06-27 09:28:51 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Navbar from "./components/Navbar";
 import Home from './pages/Home';
+import Contract from './pages/Contract';
+import Service from './pages/Service';
+import About from './pages/About';
+import Login from './pages/Login';
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
       <Navbar>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/contract" element={<Contract />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </Navbar>
 
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ children }) => {
     return (
         <div>
-            <div class="drawer drawer-end">
-                <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
-                <div class="drawer-content flex flex-col">
-                    <div class="w-full navbar bg-base-300">
-                        <div class="flex-1 px-2 mx-2 text-2xl">Clean Co.</div>
-                        <div class="flex-none lg:hidden">
-                            <label for="my-drawer-3" class="btn btn-square btn-ghost">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            <div className="drawer drawer-end">
+                <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col">
+                    <div className="w-full navbar bg-base-300">
+                        <div className="flex-1 px-2 mx-2 text-2xl">Clean Co.</div>
+                        <div className="flex-none lg:hidden">
+                            <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeLinejoin="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                             </label>
                         </div>
-                        <div class="flex-none hidden lg:block">
-                            <ul class="menu menu-horizontal">
-                                <li><a>Navbar Item 1</a></li>
-                                <li><a>Navbar Item 2</a></li>
+                        <div className="flex-none hidden lg:block">
+                            <ul className="menu menu-horizontal">
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="about">About</Link></li>
+                                <li><Link to="contract">Contract</Link></li>
+                                <li><Link to="service">Service</Link></li>
+                                <li><Link to="login">Login</Link></li>
+
                             </ul>
                         </div>
                     </div>
                     {children}
                 </div>
-                <div class="drawer-side">
-                    <label for="my-drawer-3" class="drawer-overlay"></label>
-                    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-                        <li><a>Sidebar Item 1</a></li>
-                        <li><a>Sidebar Item 2</a></li>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+                    <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="about">About</Link></li>
 
                     </ul>
 
