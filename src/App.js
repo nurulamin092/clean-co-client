ff37d72f22ed8f05f0bbaddf35a46f6e3345e244
nurulamin092 <mnurulamin092@gmial.com>
2022-06-28 20:13:16 +0600

    animisition added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import Navbar from "./components/Navbar";
 
 import { Route, Routes } from "react-router-dom";
 import { publicRouter } from "./routes/publicRouters";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
 
   return (
     <>
--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -2,10 +2,11 @@ import React from 'react';
 import BucketGirl from '../../assets/image/bucketgirl.png';
 const Landing = () => {
     return (
-        <>
+        <div className='relative'>
             <div className="hero h-screen lg:h-[60vh] bg-accent mt-16">
                 <div className="hero-content flex-col lg:flex-row">
-                    <div>
+                    <div data-aos="fade-right">
+                        <p className='text-xl'>Best Quantity</p>
                         <h1 className="text-5xl font-bold">Professional Cleaning Service</h1>
                         <p className="py-6 max-w-xl" max-w-xl>Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         <button className="btn btn-primary">Get Started</button>
@@ -15,8 +16,8 @@ const Landing = () => {
                     </div>
                 </div>
             </div>
-            <div className='rounded-2xl mx-auto mt-[-50] shadow-lg p-10 w-5/6'>
-                <h1>Get Free Estimate</h1>
+            <div className='rounded-2xl mx-auto mt-[-50px] relative z-20 bg-base-200 shadow-lg p-10 w-5/6'>
+                <h1 className='text-2xl mb-5 text-primary'>Get Free Estimate</h1>
                 <div className='grid col-span-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
                     <input
                         type="text" placeholder="Type here"
@@ -52,11 +53,12 @@ const Landing = () => {
                         type="text" placeholder="Type here"
                         class="input input-bordered w-full "
                     />
-                </div>
 
+                </div>
+                <button className='btn btn-primary mt-5'>Get a Quote</button>
 
             </div>
-        </>
+        </div>
 
     );
 };
